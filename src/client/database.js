377fc62ffd276779af08be44913b3bb1f03086fa
Gnashal <147987928+Gnashal@users.noreply.github.com>
2024-06-18 09:55:24 +0800

    Updated database

--- src/client/database.js
+++ src/client/database.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = REACT_APP_SUPABASE_URL
-const supabaseKey = REACT_APP_ANON_KEY
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+export default supabase;
