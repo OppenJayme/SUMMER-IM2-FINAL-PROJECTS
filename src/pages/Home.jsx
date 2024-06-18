c7a7fec9674568cb7eef5a47637f2bb67fb48640
Gnashal <147987928+Gnashal@users.noreply.github.com>
2024-06-18 16:55:26 +0800

    Updated Home.jsx
    Same updates ra sa App.js, akong gi move ang routes didto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
-import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import "../styles/home.css";
-import Contacts from "./Contacts";
-import About from "./AboutUs";
 
 const Home = () => {
     return (
-        <BrowserRouter>
             <header>
                 <div className="Logosect">
                     <img className="Logo" src="logo.png" alt=""/>
@@ -15,15 +12,10 @@ const Home = () => {
                     <NavLink exact to="/" className="Navbar-active" activeClassName="Navbar-active">Home</NavLink>
                     <NavLink to="/contacts" className="Navbar-active" activeClassName="Navbar-active">Contacts</NavLink>
                     <NavLink to="/aboutus" className="Navbar-active" activeClassName="Navbar-active">About us</NavLink>
-                    <button className="Login">Login</button>
+                    <button className="Login"> <Link to ="/login">Login</Link></button>
                     <button className="Register">Register</button>
                 </nav>
             </header>
-            <Routes>
-                <Route path="/contacts" element={<Contacts />} />
-                <Route path="/aboutus" element={<About />} />
-            </Routes>
-        </BrowserRouter>
     );
 };
 
