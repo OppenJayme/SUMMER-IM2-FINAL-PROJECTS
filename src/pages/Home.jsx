32753f40e7c03eb2f909497669d4dc85f55036ac
Gnashal <147987928+Gnashal@users.noreply.github.com>
2024-06-18 10:07:51 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,30 @@
-import { BrowserRouter, Link , Routes, Route} from "react-router-dom"
-import "../styles/home.css"
-import Contacts from "./Contacts"
-import About from "./AboutUs"
-
+import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
+import "../styles/home.css";
+import Contacts from "./Contacts";
+import About from "./AboutUs";
 
 const Home = () => {
     return (
         <BrowserRouter>
-        <header>
-             <div className="Logosect">
-                 <img className="Logo" src="logo.png" alt=""/>
-                 <h2 className="Logotxt">StockWise</h2>
-             </div>
-             <nav className="Navbar">
-                 <Link className="Navbar-active" to="/">Home</Link>
-                 <Link to = "/contacts">Contacts</Link>
-                 <Link to = "/aboutus">About us</Link>
-                 <button className="Login">Login</button>
-                 <button className="Register">Register</button>
-             </nav>
-         </header>
-         <Routes>
-        <Route path="/create" element={<Contacts />} />
-        <Route path="/aboutus" element={<About />} />
-         </Routes>
+            <header>
+                <div className="Logosect">
+                    <img className="Logo" src="logo.png" alt=""/>
+                    <h2 className="Logotxt">StockWise</h2>
+                </div>
+                <nav className="Navbar">
+                    <NavLink exact to="/" className="Navbar-active" activeClassName="Navbar-active">Home</NavLink>
+                    <NavLink to="/contacts" className="Navbar-active" activeClassName="Navbar-active">Contacts</NavLink>
+                    <NavLink to="/aboutus" className="Navbar-active" activeClassName="Navbar-active">About us</NavLink>
+                    <button className="Login">Login</button>
+                    <button className="Register">Register</button>
+                </nav>
+            </header>
+            <Routes>
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/aboutus" element={<About />} />
+            </Routes>
         </BrowserRouter>
-    )
-}
-export  default Home;
+    );
+};
 
+export default Home;
