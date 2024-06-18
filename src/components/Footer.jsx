4eee39c1f79a212f3264320d17a6e73e118f88a2
Gnashal <147987928+Gnashal@users.noreply.github.com>
2024-06-18 17:28:56 +0800

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+
+const Footer = () => {
+    return (
+        <footer>
+                <p class="copyright">2024 StockWise &copy; All Rights Reserved.</p>
+                <p class="tos">Terms of Service</p>
+                <p class="pap">Privacy and Policy</p>
+                <span class="icons">
+                    <a href=""><ion-icon size="large" name="logo-twitter"></ion-icon></a>
+                    <a href=""><ion-icon size="large" name="logo-facebook"></ion-icon></a>
+                    <a href=""><ion-icon size="large" name="logo-instagram"></ion-icon></a>
+                    <a href=""><ion-icon size="large" name="mail"></ion-icon></a>
+                </span>
+                </footer>
+    )
+}
+
+export default Footer;
