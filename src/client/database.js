523611973da3326c24d5a441fed7b1392392e426
ReitzX <23102910@usc.edu.ph>
2024-06-18 10:10:54 +0800

    Update database.js

--- src/client/database.js
+++ src/client/database.js
@@ -5,21 +5,3 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
 
-
-
-const userEmail;
-const companyName;
-
-
-const { data, error } = await supabase
-  .from('company_t')
-  .insert([
-    { company_email: userEmail, company_name: companyName }
-  ])
-  .select();
-
-if (error) {
-  console.error('Error inserting company details:', error.message);
-} else {
-  console.log('Company details inserted successfully:', data);
-}
