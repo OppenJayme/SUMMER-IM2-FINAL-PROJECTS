6794685d0b9300bacad51082661bad6b06a0cdad
Gnashal <147987928+Gnashal@users.noreply.github.com>
2024-06-18 21:39:52 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,29 @@
 import {Link, NavLink} from "react-router-dom";
 import "../styles/home.css";
-
+import Footer from "../components/Footer";
+import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js";
 const Home = () => {
     return (
+        <div>
             <header>
                 <div className="Logosect">
-                    <img className="Logo" src="logo.png" alt=""/>
-                    <h2 className="Logotxt">StockWise Inventory</h2>
+                    <img className="Logo" src="logo.png" alt="stockwise-logo"/>
+                    <h2 className="Logotxt">StockWise</h2>
                 </div>
                 <nav className="Navbar">
                     <NavLink exact to="/" className="Navbar-active" activeClassName="Navbar-active">Home</NavLink>
                     <NavLink to="/contacts" className="Navbar-active" activeClassName="Navbar-active">Contacts</NavLink>
                     <NavLink to="/aboutus" className="Navbar-active" activeClassName="Navbar-active">About us</NavLink>
-                    <button className="Login"> <Link to ="/login">Login</Link></button>
+                    <Link to ="/login"><button className="Login">Login</button></Link>
                     <button className="Register">Register</button>
                 </nav>
             </header>
+            <Footer/>
+        </div>
+
+                
     );
 };
 
 export default Home;
+    
