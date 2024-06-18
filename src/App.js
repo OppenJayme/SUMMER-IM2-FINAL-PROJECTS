3045a7fd519e310fa62a765628b29dc6e25e4cf1
Gnashal <147987928+Gnashal@users.noreply.github.com>
2024-06-18 16:54:34 +0800

    Updated App.js
    Ako gi butang tanan routes sa app.js kay para dili mag double sa Home, nya para limpyo rsd ang home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home"
+import Contacts from "./pages/Contacts";
+import About from "./pages/AboutUs";
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Home/>
+      <Routes>
+                <Route path="/*" element={<Home />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/aboutus" element={<About />}  />
+                <Route path="/login"  element = {<Login/>}/>
+    </Routes>
+    </Router>
   );
 }
-
 export default App;
