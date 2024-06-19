4282dd74f225eced94670c26dfb67c2e290058c9
Charlito1232 <23100750@usc.edu.ph>
2024-06-19 08:45:51 +0800

    Update landingPageCard.jsx

--- src/components/landingPageCard.jsx
+++ src/components/landingPageCard.jsx
@@ -1,7 +1,18 @@
 
 const LandingPageCard = () => {
     return (
-      
+      <div class="content">
+        <h1>Ready to Create Changes?</h1>
+        <h2>Streamline your inventory. Simplify your Life</h2>
+        <div class="imgae-grid">
+            <img class="firstimg" src="1.png" alt="123">
+            <img class="secondimg" src="2.png" alt="123">
+            <img class="thirdimg" src="3.png" alt="123">
+            <img class="fourthimg" src="4.png" alt="123">
+            <img class="fifthimg" src="5.png" alt="123">
+        </div>
+        <button class="BtnRegisterCompany">Register Company</button>
+    </div>
     )
 }
 
