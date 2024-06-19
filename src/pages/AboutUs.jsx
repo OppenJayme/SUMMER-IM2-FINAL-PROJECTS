74f2454829635c97defdb41bd44338492ed5a23c
Kyer Ivan <156319128+OppenJayme@users.noreply.github.com>
2024-06-19 10:05:37 +0800

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,25 @@
-
+import Footer from '../components/Footer'
+import { NavLink, Link } from 'react-router-dom';
 
 const About = () => {
 
     return (
-        <h1>About Us</h1>
+        <div>
+        <header>
+            <div className="Logosect">
+                <img className="Logo" src="logo.png" alt="stockwise-logo"/>
+                <h2 className="Logotxt">StockWise</h2>
+            </div>
+            <nav className="Navbar">
+                <NavLink exact to="/" className="Navbar-active" activeClassName="Navbar-active">Home</NavLink>
+                <NavLink to="/contacts" className="Navbar-active" activeClassName="Navbar-active">Contacts</NavLink>
+                <NavLink to="/aboutus" className="Navbar-active" activeClassName="Navbar-active">About us</NavLink>
+                <Link to ="/login"><button className="Login">Login</button></Link>
+                <button className="Register">Register</button>
+            </nav>
+        </header>
+        <Footer/>
+    </div>
     )
 }
 
