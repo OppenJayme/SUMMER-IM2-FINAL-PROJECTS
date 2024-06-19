6ae5b6d06fe7b7d2bfb2b7963cfb0d32866fe8d2
Kyer Ivan <156319128+OppenJayme@users.noreply.github.com>
2024-06-19 10:05:52 +0800

    Update landingPageCard.jsx

--- src/components/landingPageCard.jsx
+++ src/components/landingPageCard.jsx
@@ -1,20 +1,25 @@
+import firstImg from '../styles/images/1.png';
+import secondImg from '../styles/images/2.png';
+import thirdImg from '../styles/images/3.png';
+import fourthImg from '../styles/images/4.png';
+import fifthImg from '../styles/images/5.png';
 
 const LandingPageCard = () => {
     return (
-      <div class="content">
+      <div className="content">
         <h1>Ready to Create Changes?</h1>
         <h2>Streamline your inventory. Simplify your Life</h2>
-        <div class="imgae-grid">
-            <img class="firstimg" src="1.png" alt="123">
-            <img class="secondimg" src="2.png" alt="123">
-            <img class="thirdimg" src="3.png" alt="123">
-            <img class="fourthimg" src="4.png" alt="123">
-            <img class="fifthimg" src="5.png" alt="123">
+        <div className="image-grid">
+            <img className="firstimg" src={firstImg} alt="First" />
+            <img className="secondimg" src={secondImg} alt="Second" />
+            <img className="thirdimg" src={thirdImg} alt="Third" />
+            <img className="fourthimg" src={fourthImg} alt="Fourth" />
+            <img className="fifthimg" src={fifthImg} alt="Fifth" />
         </div>
-        <button class="BtnRegisterCompany">Register Company</button>
+        <button className="BtnRegisterCompany">Register Company</button>
     </div>
-    )
+    );
 }
 
-
 export default LandingPageCard;
+
