651cf1b0240e2022c4407233667faedca1c043d5
Kyer Ivan <156319128+OppenJayme@users.noreply.github.com>
2024-06-19 10:05:25 +0800

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,25 @@
-
+import { NavLink, Link } from 'react-router-dom';
+import Footer from '../components/Footer'
 
 const Contacts = () => {
 
     return (
-        <h1>Contacts</h1>
+        <div>
+            <header>
+                <div className="Logosect">
+                    <img className="Logo" src="logo.png" alt="stockwise-logo"/>
+                    <h2 className="Logotxt">StockWise</h2>
+                </div>
+                <nav className="Navbar">
+                    <NavLink exact to="/" className="Navbar-active" activeClassName="Navbar-active">Home</NavLink>
+                    <NavLink to="/contacts" className="Navbar-active" activeClassName="Navbar-active">Contacts</NavLink>
+                    <NavLink to="/aboutus" className="Navbar-active" activeClassName="Navbar-active">About us</NavLink>
+                    <Link to ="/login"><button className="Login">Login</button></Link>
+                    <button className="Register">Register</button>
+                </nav>
+            </header>
+            <Footer/>
+        </div>
     )
 }
 
