de4aee87e20940ec1757594f8eed01b87e211e6e
Charlito1232 <23100750@usc.edu.ph>
2024-06-20 10:23:13 +0800

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,18 +6,20 @@ const About = () => {
     return (
         <div>
         <header>
-            <div className="Logosect">
-                <img className="Logo" src="logo.png" alt="stockwise-logo"/>
-                <h2 className="Logotxt">StockWise</h2>
-            </div>
-            <nav className="Navbar">
-                <NavLink exact to="/" className="Navbar-active" activeClassName="Navbar-active">Home</NavLink>
-                <NavLink to="/contacts" className="Navbar-active" activeClassName="Navbar-active">Contacts</NavLink>
-                <NavLink to="/aboutus" className="Navbar-active" activeClassName="Navbar-active">About us</NavLink>
-                <Link to ="/login"><button className="Login">Login</button></Link>
-                <button className="Register">Register</button>
-            </nav>
-        </header>
+                <div className="Logosect">
+                    <img className="Logo" src="logo.png" alt="stockwise-logo"/>
+                    <h2 className="Logotxt">StockWise</h2>
+                </div>
+                <nav className="Navbar">
+                    <NavLink exact to="/" className="Navbar-active" activeClassName="Navbar-active">Home</NavLink>
+                    <NavLink to="/contacts" className="Navbar-active" activeClassName="Navbar-active">Contacts</NavLink>
+                    <NavLink to="/aboutus" className="Navbar-active" activeClassName="Navbar-active">About us</NavLink>
+                </nav>
+                <div className="Login-Register">
+                    <Link to ="/login"><button className="Login">Login</button></Link>
+                    <Link to = "/registration"><button className="Register">Register</button></Link>
+                </div>
+            </header>
         <Footer/>
     </div>
     )
