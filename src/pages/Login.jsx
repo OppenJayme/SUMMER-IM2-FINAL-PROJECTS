ac676eb3ef6a0aed9715d10974c634f87f083759
Charlito1232 <23100750@usc.edu.ph>
2024-06-21 08:21:11 +0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,47 @@
+import Logo from '../styles/images/logo.png';
+import Footer from '../components/Footer'
+import { NavLink, Link } from 'react-router-dom';
+import "../styles/login.css"
+import loginimg from "../styles/images/6.png"
+
 const Login = () => {
     return (
-       <h1>Login</h1>
+        <>
+        <header>
+            <div className="Logosect">
+                <img className="Logo" src={Logo} alt="stockwise-logo"/>
+                <h2 className="Logotxt">StockWise</h2>
+            </div>
+            <nav className="Navbar">
+                <NavLink exact to="/" className="Navbar-active" activeClassName="Navbar-active">Home</NavLink>
+                <NavLink to="/contacts" className="Navbar-active" activeClassName="Navbar-active">Contacts</NavLink>
+                <NavLink to="/aboutus" className="Navbar-active" activeClassName="Navbar-active">About us</NavLink>
+            </nav>
+            <div className="Login-Register">
+                <Link to ="/login"><button className="Login">Login</button></Link>
+                <Link to = "/registration"><button className="Register">Register</button></Link>
+            </div>
+        </header>
+
+        <div class="login">
+        <h1>Login</h1>
+        <form>
+          <div class="input-container">
+            <i class="bi bi-envelope input-icon"></i>
+            <input class="input-login" type="text" placeholder="Your business email"/>
+          </div>
+          <div class="input-container">
+            <i class="bi bi-unlock input-icon"></i>
+            <input class="input-password" type="password" placeholder="Your password"/>
+          </div>
+          <p>No account? No Problem <a href="">Sign up here!</a></p>
+          <button class="BtnSubmit" type="submit">Login</button>
+        </form>
+        <img class="imgbackground" src={loginimg} alt="123"/>
+      </div>
+
+        <Footer/>
+        </>
     )
 }
 
