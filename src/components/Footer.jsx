9dcf976a0d96236cb5025bd7cdc74aaee50d110b
Charlito1232 <23100750@usc.edu.ph>
2024-06-21 08:28:06 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <footer>
-                <p class="copyright">2024 StockWise &copy; All Rights Reserved.</p>
+                <p class="copyright">2024 StockWise &copy; All Rights Reserved. version 2.0</p>
                 <p class="tos">Terms of Service</p>
                 <p class="pap">Privacy and Policy</p>
                 <span class="icons">
