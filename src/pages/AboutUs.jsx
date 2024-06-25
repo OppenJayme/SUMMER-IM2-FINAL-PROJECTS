52d94599ffbff321e0df6911fe8aa8359646fb76
CodingWithCharles <23100750@usc.edu.ph>
2024-06-26 00:40:34 +0800

    asd

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Logo from '../styles/images/logo.png';
 import Footer from '../components/Footer';
 import { NavLink, Link } from 'react-router-dom';
-import "../styles/aboutus.css";
 import imgpic1 from '../styles/images/For-website-image-8.png';
 import imgpic2 from '../styles/images/GettyImages-142227958_optimized.jpg';
 import imgpic3 from '../styles/images/Web_150DPI-20190927_10th_Floor_Conference_Room_2_v1.jpg';
+import "../styles/aboutus.css";
 
 const About = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
@@ -36,26 +36,26 @@ const About = () => {
                 </div>
             </header>
 
-            <div className="pic-container">
-                <div className="slideshow">
-                    {slides.map((slide, index) => (
-                        <div
-                            key={index}
-                            className={`slide ${currentSlide === index ? 'active' : ''}`}
-                            style={{ backgroundImage: `url(${slide})` }}
-                        ></div>
-                    ))}
+            <div className='content-container'>
+                <div className="pic-container">
+                    <div className="slideshow">
+                        {slides.map((slide, index) => (
+                            <div
+                                key={index}
+                                className={`slide ${currentSlide === index ? 'active' : ''}`}
+                                style={{ backgroundImage: `url(${slide})` }}
+                            ></div>
+                        ))}
+                    </div>
+                    <button href="#register" className="BtnRegister">
+                    Register Company</button>
                 </div>
-                <a href="#register" className="BtnRegister">
-                Register Company</a>
+
+                <div className="text-container">
+                    <p>  StockWise stands at the forefront of Inventory Management Systems (IMS), dedicated to revolutionizing how businesses manage their inventory. With a keen focus on efficiency, precision, and profitability, StockWise offers a comprehensive suite of tools and features tailored to meet the diverse needs of businesses across industries. At StockWise, we are committed to excellence in every aspect of our platform. From user experience to functionality, we strive to deliver a seamless and intuitive inventory management solution that empowers businesses to thrive in today's competitive marketplace. Our cloud-based platform harnesses the power of cutting-edge technology to provide businesses with unparalleled flexibility, scalability, and accessibility. With StockWise, you can access your inventory data anytime, anywhere, from any device, ensuring that you stay connected and in control at all times. StockWise is equipped with a wide range of robust features designed to streamline and optimize every aspect of inventory control. From real-time inventory tracking to customizable stock alerts, our platform empowers businesses to make informed decisions, minimize stockouts, and maximize profitability.</p>
+                </div>
             </div>
 
-            <div className="about-us-text">
-                <h1>ABOUT US</h1>
-            </div>
-            <div className ="text-about-us">
-                <p>  StockWise stands at the forefront of Inventory Management Systems (IMS), dedicated to revolutionizing how businesses manage their inventory. With a keen focus on efficiency, precision, and profitability, StockWise offers a comprehensive suite of tools and features tailored to meet the diverse needs of businesses across industries. At StockWise, we are committed to excellence in every aspect of our platform. From user experience to functionality, we strive to deliver a seamless and intuitive inventory management solution that empowers businesses to thrive in today's competitive marketplace. Our cloud-based platform harnesses the power of cutting-edge technology to provide businesses with unparalleled flexibility, scalability, and accessibility. With StockWise, you can access your inventory data anytime, anywhere, from any device, ensuring that you stay connected and in control at all times. StockWise is equipped with a wide range of robust features designed to streamline and optimize every aspect of inventory control. From real-time inventory tracking to customizable stock alerts, our platform empowers businesses to make informed decisions, minimize stockouts, and maximize profitability.</p>
-            </div>
             <Footer />
         </div>
     );
