1f60bc79aaefb60d7bde5262b25bd02fbaa9637c
ReitzX <23102910@usc.edu.ph>
2024-06-27 10:20:40 +0800

    Update Items.jsx

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,11 +1,14 @@
 import SideNav from "../components/SideNav";
-
+import "../styles/items.css";
 const Items = () => {
     return (
         <>
         <SideNav/>
-         <h1>Items</h1>
+        <div className="items-text">ITEMS</div>
+         <div className="search-container">
+                <input className="search-box" type="text" placeholder="Search"/>
+            </div>
         </>
-    )
-}
+    );
+}   
 export default  Items;
