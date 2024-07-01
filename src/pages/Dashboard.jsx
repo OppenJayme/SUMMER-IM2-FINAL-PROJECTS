e435b1e377c4c967160da65867289ab6491ac72e
Gnashal <yousifceballos@gmail.com>
2024-07-02 07:57:46 +0800

    gi delete nako ang dashboard components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,22 +10,22 @@ const Dashboard = () => {
             <SideNav/>
             <div className="dashboard_content">
             <div className="dashboard_box">
-                <TotalMarketValue />
+                <h1>Total Categories</h1>
             </div>
             <div className="dashboard_box">
-                <TotalCategories />
+                <h1>Total Employees</h1>
             </div>
             <div className="dashboard_box">
-                <TotalEmployees />
+              <h1>Total Sales</h1>
             </div>
             <div className="dashboard_box">
-                <TotalItems />
+                <h1>Total Items</h1>
             </div>
             <div className="dashboard_box">
-                <TotalSales />
+                <h1>Total Market Value</h1>
             </div>
             <div className="dashboard_box">
-                <TotalMarketRevenue />
+                <h1>Total Market Revenue</h1>
             </div>
         </div>
         </>
