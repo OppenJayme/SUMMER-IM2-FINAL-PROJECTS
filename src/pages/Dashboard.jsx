2a5a8543987b8ca5dc7bac3dded14c547428da16
CharlitoPogi <2310075@usc.edu.ph>
2024-07-02 09:44:18 +0800

    123123

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,62 +1,55 @@
 import "../styles/dashboard.css";
 import SideNav from "../components/SideNav";
-
+import firstImg from '../styles/images/1.png';
 const Dashboard = () => {
 
     return (
         <>
             <SideNav/>
+                    <div className="dashboard_content">
+                        <div className="main_content_container">
+                            <h1>Dashboard</h1>
 
-            <div className="dashboard_content">
-<<<<<<< HEAD
-                <div className="category_container">
-                    
-                    <div className="dashboard_box">
-                        <h1>TotalCategories</h1>
-                    </div>
+                            <div className="container0">
 
-                    <div className="dashboard_box">
-                        <h1>TotalEmployees</h1>
-                    </div>
+                                <div className="dashboard_box">
+                                    <h1>TotalCategories</h1>
+                                    <img src={firstImg} alt="" />
+                                </div>
 
-                    <div className="dashboard_box">
-                        <h1>TotalSales</h1>
-                    </div>
+                                <div className="dashboard_box">
+                                    <h1>TotalEmployees</h1>
+                                    <img src={firstImg} alt="" />
+                                </div>
 
-                    <div className="dashboard_box">
-                        <h1>TotalItems</h1>
-                    </div>
+                                <div className="dashboard_box">
+                                    <h1>TotalSales</h1>
+                                    <img src={firstImg} alt="" />
+                                </div>
 
-                    <div className="dashboard_box">
-                        <h1>TotalMarketValue</h1>
-                    </div>
+                                <div className="dashboard_box">
+                                    <h1>TotalItems</h1>
+                                    <img src={firstImg} alt="" />
+                                </div>
 
-                    <div className="dashboard_box">
-                        <h1>TotalMarketRevenue</h1>
+                            </div>
+
+
+                            <div className="container1">
+
+                                <div className="dashboard_box">
+                                    <h1>TotalMarketValue</h1>
+                                    <h2>VALUE HERE!</h2>
+                                </div>
+
+                                <div className="dashboard_box">
+                                    <h1>TotalMarketRevenue</h1>
+                                    <h2>VALUE HERE!</h2>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-=======
-            <div className="dashboard_box">
-                <h1>Total Categories</h1>
-            </div>
-            <div className="dashboard_box">
-                <h1>Total Employees</h1>
-            </div>
-            <div className="dashboard_box">
-              <h1>Total Sales</h1>
-            </div>
-            <div className="dashboard_box">
-                <h1>Total Items</h1>
-            </div>
-            <div className="dashboard_box">
-                <h1>Total Market Value</h1>
-            </div>
-            <div className="dashboard_box">
-                <h1>Total Market Revenue</h1>
-            </div>
-        </div>
->>>>>>> 4900582545c79837549160aa0e31a4261c85b180
         </>
     );
 };
