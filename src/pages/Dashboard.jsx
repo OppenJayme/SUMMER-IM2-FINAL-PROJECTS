e821362581c76a7ffdb235115a24c4fee038ec39
CharlitoPogi <2310075@usc.edu.ph>
2024-07-02 08:11:05 +0800

    asdasdasdasd

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,32 +2,40 @@ import "../styles/dashboard.css";
 import { TotalMarketValue, TotalCategories, TotalEmployees, TotalItems, TotalMarketRevenue, TotalSales} from "../components/DashboardComps";
 import SideNav from "../components/SideNav";
 
-
 const Dashboard = () => {
 
     return (
         <>
             <SideNav/>
+
             <div className="dashboard_content">
-            <div className="dashboard_box">
-                <TotalMarketValue />
+                <div className="category_container">
+                    
+                    <div className="dashboard_box">
+                        <h1>TotalCategories</h1>
+                    </div>
+
+                    <div className="dashboard_box">
+                        <h1>TotalEmployees</h1>
+                    </div>
+
+                    <div className="dashboard_box">
+                        <h1>TotalSales</h1>
+                    </div>
+
+                    <div className="dashboard_box">
+                        <h1>TotalItems</h1>
+                    </div>
+
+                    <div className="dashboard_box">
+                        <h1>TotalMarketValue</h1>
+                    </div>
+
+                    <div className="dashboard_box">
+                        <h1>TotalMarketRevenue</h1>
+                    </div>
+                </div>
             </div>
-            <div className="dashboard_box">
-                <TotalCategories />
-            </div>
-            <div className="dashboard_box">
-                <TotalEmployees />
-            </div>
-            <div className="dashboard_box">
-                <TotalItems />
-            </div>
-            <div className="dashboard_box">
-                <TotalSales />
-            </div>
-            <div className="dashboard_box">
-                <TotalMarketRevenue />
-            </div>
-        </div>
         </>
     );
 };
