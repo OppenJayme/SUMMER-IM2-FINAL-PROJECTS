2f0ab18263046ddd51737c6849dc9dd4e5483151
Charles <23100750@usc.edu.ph>
2024-07-08 10:07:54 +0800

    Item Fix

--- src/components/ItemInspect.jsx
+++ src/components/ItemInspect.jsx
@@ -10,23 +10,28 @@ const ItemInspect = ({ show, onClose, item }) => {
     return (
         <div className="inspect-modal-overlay">
             <div className="inspect-modal-content">
-                <div className="inspect-modal-content_left">
-                    <img src={item.product_t.image_path} alt="" />
 
+                <div className="inspect-modal-content_left">
+                    <img src={item.product_t.image_path} alt="image" />
                 </div>
+
                 <div className="inspect-modal-content_right">
-                    <h2>{item.product_t.product_name}</h2>
-                    <p>Category: {item.product_t.category}</p>
-                    <p>Quantity: {item.product_t.product_quantity}</p>
-                    <p>Sold: {item.productSale}</p>
-                    <p>Price: ${item.product_t.product_price}.00</p>
-                    <p>Supplier: {item.product_t.suppliername}</p>
-                    <p>Date Added: {item.product_t.dateadded}</p>
+                    <div className="close">
+                            <i onClick={onClose} class="bi bi-x-lg"></i>
+                    </div>
+                        <h2>{item.product_t.product_name}</h2>
+                        <p>Category: {item.product_t.category}</p>
+                        <p>Quantity: {item.product_t.product_quantity}</p>
+                        <p>Sold: {item.productSale}</p>
+                        <p>Price: ${item.product_t.product_price}.00</p>
+                        <p>Supplier: {item.product_t.suppliername}</p>
+                        <p>Date Added: {item.product_t.dateadded}</p>
+                    <div className="Btns">
+                        <button className='Btnfirst' onClick={onClose}>Update</button>
+                        <button className='Btnsecond' onClick={onClose}>Delete</button>
+                    </div>
                 </div>
             </div>
-
-                <button onClick={onClose}>Close</button>
-            
         </div>
     );
 };
